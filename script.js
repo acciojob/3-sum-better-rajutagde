059333a8d5db6b21fed3bd2adeeeb50a9ca4c4f5
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-18 15:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function threeSum(arr, target) {
-// write your code here
-  
   arr.sort((a, b) => a - b); // Sort the array in non-decreasing order
   let closestSum = Infinity;
   for (let i = 0; i < arr.length - 2; i++) {
